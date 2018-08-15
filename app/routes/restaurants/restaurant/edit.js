7d3b0908d6294c195b26a3e83d169830fe7b63b3
Chris Gallo <cgallo@fnba.com>
2018-08-15 16:16:22 -0400

    claiming food

--- app/routes/restaurants/restaurant/edit.js
+++ app/routes/restaurants/restaurant/edit.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(params){
+    return Ember.RSVP.hash({
+      restaurant: this.store.findRecord('restaurant', params.restaurant_id),
+      foods: this.store.findAll('food')
+    })
+  }
 });
